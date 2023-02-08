de64680d86e307dd71eaa2e7455ffccc79df63e2
Josh Tell <jtell1997@gmail.com>
2023-02-08 23:47:39 +1300

    Added hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,8 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  config.hosts << "review-project-production.up.railway.app"
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
