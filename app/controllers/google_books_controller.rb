91a8baf4444e2dc28053f16bf216d98a60b4ac0b
Josh Tell <jtell1997@gmail.com>
2024-02-10 15:36:21 +1300

    Change search

--- app/controllers/google_books_controller.rb
+++ app/controllers/google_books_controller.rb
@@ -2,24 +2,33 @@
 
 class GoogleBooksController < ApplicationController
   def index
-    @google_books = []
-
     # Books that have an average review of 4 or higher
     @popular_books = Book.joins('LEFT OUTER JOIN reviews ON reviews.book_id = books.id')
                          .group('books.id')
                          .having('AVG(reviews.rating) >= ?', 4)
                          .where('reviews.id IS NOT NULL')
+  end
 
-    existing_book = Book.where('LOWER(title) LIKE ?', "%#{params[:title]}%").first
-    if existing_book.present?
-      @existing_book = existing_book
-      @google_books.append(@existing_book)
-    end
+  def search
+    @search_results = []
 
-    google_book_results = GoogleBooks.search(params[:title], { count: 10 - @google_books.length })
+    existing_book = Book.where('LOWER(title) LIKE ?', "%#{params[:google_books][:title]}%").first
+    @search_results.append(existing_book) if existing_book.present?
+
+    google_book_results = GoogleBooks.search(params[:google_books][:title], { count: 10 - @search_results.length })
 
     google_book_results.each do |gbook|
-      @google_books.append(Book.new(title: gbook.title, author: gbook.authors, isbn: gbook.isbn)) if gbook.isbn.present?
+      next unless gbook.isbn.present?
+
+      new_book = Book.new(
+        title: gbook.title,
+        author: gbook.authors,
+        description: gbook.description,
+        isbn: gbook.isbn
+      )
+      new_book.image_link = gbook.image_link(zoom: 5, curl: true) if gbook.image_link(zoom: 5, curl: true).present?
+
+      @search_results.append(new_book)
     end
   end
 end
